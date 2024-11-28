03b09bca4ba4c8b859d8e4204b0c44b9f3160fd8
dentalFloss <northeastflo@gmail.com>
2024-11-28 15:25:52 -0500

    Added evaluation

--- scripts/bayes_gnn.sh
+++ scripts/bayes_gnn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --time=15:00:00
-#SBATCH --mem=10G
+#SBATCH --time=30:00:00
+#SBATCH --mem=50G
 #SBATCH --cpus-per-task=4
 #SBATCH --gpus-per-node=1
 
--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -79,3 +79,79 @@ def voltage_angle_difference(voltage_angle, voltage_angle_limits):
     angle_lower_bound, angle_upper_bound = voltage_angle_limits
     angle_violations = torch.sum((voltage_angle < angle_lower_bound[0]) | (voltage_angle > angle_upper_bound[0]))
     return angle_violations.item()
+
+def compute_metrics(data, out, branch_powers_ac_line, branch_powers_transformer, optimizer, BRANCH_FEATURE_INDICES, data_type):
+    pf_pred_ac_line, qf_pred_ac_line, pt_pred_ac_line, qt_pred_ac_line = branch_powers_ac_line
+    pf_pred_transformer, qf_pred_transformer, pt_pred_transformer, qt_pred_transformer = branch_powers_transformer
+    
+    edge_label_ac_line = data['bus', 'ac_line', 'bus'].edge_label
+    pf_true_ac_line = edge_label_ac_line[:, 0]
+    qf_true_ac_line = edge_label_ac_line[:, 1]
+    pt_true_ac_line = edge_label_ac_line[:, 2]
+    qt_true_ac_line = edge_label_ac_line[:, 3]
+    
+    edge_label_transformer = data['bus', 'transformer', 'bus'].edge_label
+    pf_true_transformer = edge_label_transformer[:, 0]
+    qf_true_transformer = edge_label_transformer[:, 1]
+    pt_true_transformer = edge_label_transformer[:, 2]
+    qt_true_transformer = edge_label_transformer[:, 3]
+    
+    # LR
+    current_lr = optimizer.param_groups[0]['lr']
+
+    # TRMAE
+    trmae_to_bus_real_batch = compute_trmae(pt_true_ac_line, pt_pred_ac_line)
+    trmae_to_bus_reactive_batch = compute_trmae(qt_true_ac_line, qt_pred_ac_line)
+    trmae_from_bus_real_batch = compute_trmae(pf_true_ac_line, pf_pred_ac_line)
+    trmae_from_bus_reactive_batch = compute_trmae(qf_true_ac_line, qf_pred_ac_line)
+    
+    trmae_generator_withdrawn_to_bus_real_batch = compute_trmae(pt_true_transformer, pt_pred_transformer)
+    trmae_generator_withdrawn_to_bus_reactive_batch = compute_trmae(qt_true_transformer, qt_pred_transformer)
+    trmae_generator_withdrawn_from_bus_real_batch = compute_trmae(pf_true_transformer, pf_pred_transformer)
+    trmae_generator_withdrawn_from_bus_reactive_batch = compute_trmae(qf_true_transformer, qf_pred_transformer)
+                    
+    # Voltage angles
+    voltage_angle_bus = out['bus'][:, 0] # theta in radians
+    indices_bus_angmin = BRANCH_FEATURE_INDICES['ac_line']['angmin']
+    indices_bus_angmax = BRANCH_FEATURE_INDICES['ac_line']['angmax']
+    edge_attr_ac_line = data['bus', 'ac_line', 'bus'].edge_attr
+    bus_angmin = edge_attr_ac_line[:, indices_bus_angmin]
+    bus_angmax = edge_attr_ac_line[:, indices_bus_angmax]
+
+    voltage_angle_node = out['generator'][:, 0] # theta in radians
+    indices_node_angmin = BRANCH_FEATURE_INDICES['transformer']['angmin']
+    indices_node_angmax = BRANCH_FEATURE_INDICES['transformer']['angmax']
+    edge_attr_transformer = data['bus', 'transformer', 'bus'].edge_attr
+    node_angmin = edge_attr_transformer[:, indices_node_angmin]
+    node_angmax = edge_attr_transformer[:, indices_node_angmax]
+                    
+    # Thermal limits
+    indices_ac_line = BRANCH_FEATURE_INDICES['ac_line']
+    rate_a_ac_line = edge_attr_ac_line[:, indices_ac_line['rate_a']]
+    indices_transformer = BRANCH_FEATURE_INDICES['transformer']
+    rate_a_transformer = edge_attr_transformer[:, indices_transformer['rate_a']]
+    
+    # Power balance
+    power_balance = power_balance_violation(pf_true_ac_line, pt_true_ac_line, pf_pred_ac_line, pt_pred_ac_line, pf_true_transformer, pt_true_transformer, pf_pred_transformer, pt_pred_transformer)
+    
+    metrics = {
+        data_type + '_lr': current_lr,
+        data_type + '_trmae_to_bus_real_batch': trmae_to_bus_real_batch,
+        data_type + '_trmae_to_bus_reactive_batch': trmae_to_bus_reactive_batch,
+        data_type + '_trmae_from_bus_real_batch': trmae_from_bus_real_batch,
+        data_type + '_trmae_from_bus_reactive_batch': trmae_from_bus_reactive_batch,
+        data_type + '_trmae_bus_total': trmae_to_bus_real_batch + trmae_to_bus_reactive_batch + trmae_from_bus_real_batch + trmae_from_bus_reactive_batch,
+        data_type + '_trmae_generator_withdrawn_to_bus_real_batch': trmae_generator_withdrawn_to_bus_real_batch,
+        data_type + '_trmae_generator_withdrawn_to_bus_reactive_batch': trmae_generator_withdrawn_to_bus_reactive_batch,
+        data_type + '_trmae_generator_withdrawn_from_bus_real_batch': trmae_generator_withdrawn_from_bus_real_batch,
+        data_type + '_trmae_generator_withdrawn_from_bus_reactive_batch': trmae_generator_withdrawn_from_bus_reactive_batch,
+        data_type + '_trma_generator_total': trmae_generator_withdrawn_to_bus_real_batch + trmae_generator_withdrawn_to_bus_reactive_batch + trmae_generator_withdrawn_from_bus_real_batch + trmae_generator_withdrawn_from_bus_reactive_batch,
+        data_type + '_power_balance_violation_ac_line': power_balance[0],
+        data_type + '_power_balance_violation_trans': power_balance[1],
+        data_type + '_voltage_angle_difference_node': voltage_angle_difference(voltage_angle_node, [node_angmin, node_angmax]),
+        data_type + '_voltage_angle_difference_bus': voltage_angle_difference(voltage_angle_bus, [bus_angmin, bus_angmax]),
+        data_type + '_thermal_limits': thermal_limit_violation(pf_pred_ac_line, qf_pred_ac_line, pf_pred_transformer, qf_pred_transformer, rate_a_ac_line, rate_a_transformer),
+    }
+    
+    return metrics
+                    
